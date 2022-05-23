0650fa004fb5c6d71f68cfbde4d1ec261237ad9a
Nayan P Ingale <97461960+NayanIngale1@users.noreply.github.com>
2022-05-23 18:05:17 +0530

    update

--- server/src/configs/googleOauth.js
+++ server/src/configs/googleOauth.js
@@ -1,24 +1,20 @@
-var GoogleStrategy = require("passport-google-oauth2").Strategy;
 require("dotenv").config();
+const User = require("../models/Users.model");
+var GoogleStrategy = require("passport-google-oauth2").Strategy;
 const passport = require("passport");
-const { v4: uuidv4 } = require("uuid");
-const User = require("../models/user.model");
+const { v4 } = require("uuid");
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/auth/google/callback",
+      callbackURL:
+        "http://localhost:4000/auth/google/callback",
     },
-
-    async function (accessToken, refreshToken, profile, cb) {
-      //   console.log("profile:", profile);
-
-      let user = await User.findOne({ email: profile?._json?.email })
-        .lean()
-        .exec();
-
+    async function (request, accessToken, refreshToken, profile, done) {
+      let user = await User.findOne({ email: profile?.email }).lean().exec();
+      // console.log(profile);
       if (!user) {
         user = await User.create({
           firstName: profile._json.name.split(" ")[0],
@@ -28,7 +24,7 @@ passport.use(
         });
       }
       console.log(user);
-      return cb(null, { user });
+      return done(null, user);
     }
   )
 );
